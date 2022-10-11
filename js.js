e008aa5f8daa90b24a4e92d2e4ab6a8ceadfecba
joaovitourr <joaomonster2003@gmail.com>
2022-10-11 12:48:48 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById("enviar").addEventListener('click', function(event){
+    event.preventDefault();
+
+});
+
+function limparForm()
+{
+    document.getElementById("email").value = "";
+}
+
